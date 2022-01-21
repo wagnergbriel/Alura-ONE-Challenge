bab08b618af320e636506733fc65c15abf797990
wagnergbriel <wagnergabriel220596@gmail.com>
2022-01-20 22:51:50 -0300

    feat: adicionar as funcoes do projeto

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-const msgOriginal  = document.getElementById('msg-original');
+function CriptografarMensagem() {
+    const msgOriginal = document.getElementById('msg-original');
+    const listaDePalavras = msgOriginal.value.split('');
 
+    for (var i = 0; i < listaDePalavras.length; i++) {
+        if (listaDePalavras[i] == "e" || listaDePalavras[i] == "e".toUpperCase()) listaDePalavras[i] = "enter";
+    }
 
-function CriptografarMesagem(){
-    return console.log(msgOriginal.value)
+    let msgCodificada = document.getElementById('msg-codificada').innerHTML = listaDePalavras.join('');
+    return msgCodificada;
+}
+
+function DescriptografarMensagem() {
+    return null;
+}
+
+function CopiarMensagem() {
+    return null;
 }
