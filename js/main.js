a9b008169c29f345679e1f9b1e964af88168a290
Wagner Gabriel <wagnergabbriel220596@gmail.com>
2022-01-22 17:49:45 -0300

    feat: adicionar conclucao de CriptogrfarMensagem()

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ function CriptografarMensagem() {
 
     for (var i = 0; i < listaDePalavras.length; i++) {
         if (listaDePalavras[i] == "e" || listaDePalavras[i] == "e".toUpperCase()) listaDePalavras[i] = "enter";
+        if (listaDePalavras[i] == "i" || listaDePalavras[i] == "i".toUpperCase()) listaDePalavras[i] = "imes";
+        if (listaDePalavras[i] == "a" || listaDePalavras[i] == "a".toUpperCase()) listaDePalavras[i] = "ai";
+        if (listaDePalavras[i] == "o" || listaDePalavras[i] == "o".toUpperCase()) listaDePalavras[i] = "ober";
+        if (listaDePalavras[i] == "u" || listaDePalavras[i] == "u".toUpperCase()) listaDePalavras[i] = "ufat";
     }
 
     let msgCodificada = document.getElementById('msg-codificada').innerHTML = listaDePalavras.join('');
@@ -11,7 +15,19 @@ function CriptografarMensagem() {
 }
 
 function DescriptografarMensagem() {
-    return null;
+    const msgOriginal = document.getElementById('msg-original');
+    const listaDePalavras = msgOriginal.value.split('');
+
+    for (var i = 0; i < listaDePalavras.length; i++) {
+        if (listaDePalavras[i] == "e" || listaDePalavras[i] == "e".toUpperCase()) listaDePalavras[i] = "enter";
+        if (listaDePalavras[i] == "i" || listaDePalavras[i] == "i".toUpperCase()) listaDePalavras[i] = "imes";
+        if (listaDePalavras[i] == "a" || listaDePalavras[i] == "a".toUpperCase()) listaDePalavras[i] = "ai";
+        if (listaDePalavras[i] == "o" || listaDePalavras[i] == "o".toUpperCase()) listaDePalavras[i] = "ober";
+        if (listaDePalavras[i] == "u" || listaDePalavras[i] == "u".toUpperCase()) listaDePalavras[i] = "ufat";
+    }
+
+    let msgCodificada = document.getElementById('msg-codificada').innerHTML = listaDePalavras.join('');
+    return msgCodificada;
 }
 
 function CopiarMensagem() {
